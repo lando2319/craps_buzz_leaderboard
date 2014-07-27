df6da01d7f5b60443c79adb6ced0df5a16891dbe
Mike Land <mikepland@gmail.com>
2014-07-26 21:54:51 -0500

    deletes extra folder and adjusts tabs

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
 
-// Express - Using the /public folder as static assets
-app.use(express.static(__dirname + '/public'));
-
-
 // Loading our models file
 var models = require('./models.js')();
 
@@ -24,30 +20,30 @@ app.all('/*', function(req, res, next) {
 // ---------- ROUTES ----------------------
 // List all our users
 app.get('/api/users', function(req, res){
-	var result = models.userList(function(error, result){
-  	res.send(apiOutput(result));
-	});
+  var result = models.userList(function(error, result){
+  res.send(apiOutput(result));
+  });
 });
 
 // Insert new user (create account)
 app.post('/api/users/create', function(req, res){
-	console.log(req.param('name'))
-	//console.log(req.query);
-	var params = req.params;
-	var callback = function(error, result){
-  	res.send(apiOutput(result));
-	}
-	models.enterLeaderboard(req.param('name'), req.param('cashout_amount'), callback);
+  console.log(req.param('name'))
+  //console.log(req.query);
+  var params = req.params;
+  var callback = function(error, result){
+  res.send(apiOutput(result));
+  }
+  models.enterLeaderboard(req.param('name'), req.param('cashout_amount'), callback);
 });
 
 // ---------- Start server ----------------------
 // Start the (express) server on port 3000
 var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
 
 // ---------- HELPER ----------------------
 // Helper
 function apiOutput(data){
-	return JSON.stringify(data)
+  return JSON.stringify(data)
 }
